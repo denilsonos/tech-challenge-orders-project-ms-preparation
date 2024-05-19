a17c2920f7cf7999f36fa3be312391e26df66183
Helio Soares <helio_soares@outlook.com>
2024-05-19 17:23:25 -0300

    # Adjust for test

--- src/adapters/controllers/orders-controller.ts
+++ src/adapters/controllers/orders-controller.ts
@@ -42,7 +42,7 @@ export class OrderController implements Order {
     const result = schema.safeParse(bodyParams)
 
     if (!result.success) {
-      console.log("validation error: "+result)
+      console.log("validation error: "+ result.error)
       throw new BadRequestException('Validation error!', result.error.issues)
     }
 
